f18d54eb7b62a158e8cd618f28c979d3396d7742
asli zeynep boyar <54912300+asliboyar@users.noreply.github.com>
2020-02-15 13:26:04 -0600

    Create squares_1to5.java

---
+++ squares_1to5.java
@@ -0,0 +1,16 @@
+// with for loop
+class Main {
+  public static void main(String[] args) {
+
+    int n;
+    int square;
+    for (n=1; n<=5; n++)
+
+    {
+    square = n*n;
+    System.out.println(square + " square of " + n);
+    
+    }
+
+  }
+}
