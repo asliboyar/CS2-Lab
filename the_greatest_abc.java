b45cc149b27705b8e0e184fb9926ab163ff87e3a
asli zeynep boyar <54912300+asliboyar@users.noreply.github.com>
2020-02-15 13:24:20 -0600

    Create the_greatest_abc.java

---
+++ the_greatest_abc.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    int a, b, c;
+    a = 5;
+    b = 6;
+    c = 7;
+    if (a > b && a > c)
+        System.out.println("a is greatest " + a);
+    
+    else if (b > a && b > c)
+        System.out.println("b is greatest " + b);
+    
+    else 
+        System.out.println("c is greatest "+ c);
+    
+  }
+}
