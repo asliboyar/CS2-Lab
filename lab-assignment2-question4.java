bd6543d63c9f2057412b637d598740eb2a47428c
asli zeynep boyar <54912300+asliboyar@users.noreply.github.com>
2020-04-01 18:43:52 -0500

    Update and rename lab-assignment2-question4 to lab-assignment2-question4.java

--- lab-assignment2-question4.java
+++ lab-assignment2-question4.java
@@ -5,9 +5,7 @@ public class handshakes
     {
         int numberOFStudents;
         Scanner sc = new Scanner(System.in);
-        
         numberOFStudents = sc.nextInt();
-        
         int total = numberOFStudents * (numberOFStudents-1) / 2; 
         System.out.print("Number of Handshakes is " +total);
         sc.close();
