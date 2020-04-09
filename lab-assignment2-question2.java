6c955a75aa2f41de956be081a2969ee62e6eaaa3
asli zeynep boyar <54912300+asliboyar@users.noreply.github.com>
2020-04-09 00:00:34 -0500

    Update lab-assignment2-question2.java

--- lab-assignment2-question2.java
+++ lab-assignment2-question2.java
@@ -1,30 +1,28 @@
-import java.util.*;
-class friendlyPairs
-{
-    public static void main(String[] args)
-    {
-        int num1, num2;
-        Scanner sc = new Scanner(System.in);
-        num1 = sc.nextInt();
-        num2 = sc.nextInt();
+import java.util.Scanner;
+public class FriendlyPairs {
+    public static void main(String[] args){
+        int x, y;
+        Scanner scanner = new Scanner(System.in);
+        x = scanner.nextInt();
+        y = scanner.nextInt();
         int sum1 = 0, sum2 = 0;
-        for(int i = 1; i < num1; i++)
+        for(int i = 1; i < x; i++)
         {
-            if(num1 % i == 0)
+            if(x % i == 0)
             {
                 sum1 = sum1 + i;
             }
         }
-        for(int i = 1; i < num2; i++)
+        for(int i = 1; i < y; i++)
         {
-            if(num2 % i == 0)
+            if(y % i == 0)
             {
                 sum2 = sum2 + i;
             }
         }
-        if(sum1 == num1 && sum2 == num2)
-            System.out.print("Friendly Pair");
+        if(sum1 == x && sum2 == y)
+            System.out.print(x +" and "+ y +" are friendly pair");
         else
-            System.out.print("Not Friendly Pair");
+            System.out.print(x +" and "+ y +" are not friendly pair");
     }
 }
