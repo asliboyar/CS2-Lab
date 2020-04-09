b95ef09e819ffc9d48d5be78b39360fbf47b6379
asli zeynep boyar <54912300+asliboyar@users.noreply.github.com>
2020-04-08 23:53:48 -0500

    Update lab-assignment2-question4.java

--- lab-assignment2-question4.java
+++ lab-assignment2-question4.java
@@ -1,13 +1,11 @@
-import java.util.*;
-public class handshakes
-{
-    public static void main(String[] args)
-    {
-        int numberOFStudents;
+import java.util.Scanner;
+public class Handshakes{
+    public static void main(String[] args){
+        int numstudents;
         Scanner sc = new Scanner(System.in);
-        numberOFStudents = sc.nextInt();
-        int total = numberOFStudents * (numberOFStudents-1) / 2; 
-        System.out.print("Number of Handshakes is " +total);
+        numstudents = sc.nextInt();
+        int total = numstudents * (numstudents-1) / 2; 
+        System.out.print("Total Handshakes are " +total);
         sc.close();
     }
 }
